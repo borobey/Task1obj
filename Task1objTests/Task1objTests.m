3cbb37c36115744857b59c036828126c26f6e7d6
Admin <admin@darkstar-2.local>
2013-11-05 23:56:40 +0400

    Initial Commit

---
+++ Task1objTests/Task1objTests.m
@@ -0,0 +1,34 @@
+//
+//  Task1objTests.m
+//  Task1objTests
+//
+//  Created by Admin on 05.11.13.
+//  Copyright (c) 2013 VorobyevAlexander. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Task1objTests : XCTestCase
+
+@end
+
+@implementation Task1objTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
